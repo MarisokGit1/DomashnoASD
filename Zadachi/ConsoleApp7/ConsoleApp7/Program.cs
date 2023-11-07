0f588df7a8daef357022fc2290da98409baafb67
IvailoTonchev <ivcho465@gmail.com>
2023-11-07 08:19:45 +0200

    new change

--- Zadachi/ConsoleApp7/ConsoleApp7/Program.cs
+++ Zadachi/ConsoleApp7/ConsoleApp7/Program.cs
@@ -10,6 +10,28 @@ namespace ConsoleApp7
     {
         static void Main(string[] args)
         {
+        Console.Write("Въведете p: ");
+        int p = int.Parse(Console.ReadLine());
+
+        Console.Write("Въведете q: ");
+        int q = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{p}/{q} = ");
+
+        while (p != 0)
+        {
+            int a = (q + p - 1) / p;
+            Console.Write($"1/{a}");
+            p = p * a - q;
+            q = q * a;
+
+            if (p != 0)
+            {
+                Console.Write(" + ");
+            }
+        }
+
+        Console.WriteLine();
         }
     }
 }
